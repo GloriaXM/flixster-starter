87afd181d1df1f9a8657c4045e4ff5e5b67c1731
Gloria Ines Xiang-Yun Moudou <gloriamoudou@meta.com>
2024-06-13 15:47:02 -0700

    debug clicking load more button when searching genres

--- src/App.jsx
+++ src/App.jsx
@@ -16,21 +16,21 @@ const App = () => {
     const apiKey = import.meta.env.VITE_API_KEY;
     let response = {};
 
+    console.log(fxn);
     switch (fxn) {
       case 'now_playing':
-        console.log("searching by Now Playing");
+        console.log('Searching by now playing');
         response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=${pages}&api_key=${apiKey}`);
         break;
       case 'search':
-        console.log("Searching by Search");
+        console.log('Searching by now playing');
         response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${searchTerm}&include_adult=false&language=en-US&page=1&api_key=${apiKey}`);
         break;
       case 'by_genre':
-        console.log("Searching by genre");
-        response = await fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${genre}&language=en-US&page=1&api_key=${apiKey}`);
+        console.log('Searching by now playing');
+        response = await fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${genre}&language=en-US&page=${page}&api_key=${apiKey}`);
         break;
       default:
-        console.log("Searching by default");
         response = response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&api_key=${apiKey}`);
 
     }
@@ -44,17 +44,16 @@ const App = () => {
   }, [searchTerm]);
 
   useEffect(() => {
-    generalFetchMovies('now_playing', loadedMovies, page, '', '');
+    generalFetchMovies(apiFunctionCall, loadedMovies, page, '', genre);
   }, [page]);
 
   useEffect(() => {
-    console.log("Genre use effect")
     generalFetchMovies("by_genre", [], 1, '', genre);
   }, [genre]);
 
   const loadAdditionalPage = () => {
     setPage(page + 1);
-    generalFetchMovies('now_playing', loadedMovies, page + 1, '');
+    generalFetchMovies(apiFunctionCall, loadedMovies, page + 1, searchTerm, genre);
   }
 
   const onSubmitSearch = (searchTerm) => {
@@ -63,16 +62,19 @@ const App = () => {
 
   const goToSearchView = () => {
     setSearchTerm('');
+    setApiFunctionCall('search');
     setLoadedMovies([]);
   }
 
   const goToNowShowingView = () => {
     setPage(1);
+    setApiFunctionCall('now_playing');
     generalFetchMovies('now_playing', [], 1, '');
   }
 
   const getNewGenre = (newGenreName) => {
     setLoadedMovies([]);
+    setApiFunctionCall('by_genre');
     setGenre(newGenreName);
   }
 
